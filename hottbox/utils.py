be1ae678ca6a4e9fb58b68e67b6fb6ab7f8b247e
Ilya Kisil <ilyakisil@gmail.com>
2018-04-27 11:06:21 +0100

    Initial form of utils module

---
+++ hottbox/utils.py
@@ -0,0 +1,13 @@
+"""
+This module contains useful functions for the iterative tensor routines
+and data type conversions
+"""
+
+def decompose_group():
+    pass
+
+def pd_to_tensor():
+    pass
+
+def tensor_to_pd():
+    pass
