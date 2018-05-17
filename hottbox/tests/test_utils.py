28da3976a76dd0f1246af073b5f66b3a9c1ec41b
Ilya Kisil <ilyakisil@gmail.com>
2018-05-17 10:19:09 +0100

    Added util for quick tensor construction

--- hottbox/tests/test_utils.py
+++ hottbox/tests/test_utils.py
@@ -1,5 +1,40 @@
 from .. import version
 from ..utils import *
 
+
 def test_version():
     assert isinstance(version.__version__, str)
+
+
+def test_quick_tensor():
+    shape = (2, 3, 4)
+    true_data_arange = np.array([[[0,  1,  2,  3],
+                                  [4,  5,  6,  7],
+                                  [8,  9, 10, 11]],
+
+                                 [[12, 13, 14, 15],
+                                  [16, 17, 18, 19],
+                                  [20, 21, 22, 23]]])
+
+    true_mode_names = ["mode-{}".format(i) for i in range(len(shape))]
+    true_mode_index = None
+    true_state = [0, 1, 2]
+
+    tensor = quick_tensor(shape=shape, base='arange')
+    np.testing.assert_array_equal(tensor.data, true_data_arange)
+    assert tensor.mode_names == true_mode_names
+    assert tensor.current_state == true_state
+    for mode in tensor.modes:
+        assert mode.index is true_mode_index
+
+    tensor = quick_tensor(shape=shape, base='undefined')
+    np.testing.assert_array_equal(tensor.data, true_data_arange)
+
+    tensor = quick_tensor(shape=shape, base='randn')
+    assert tensor.shape == shape
+
+    tensor = quick_tensor(shape=shape, base='rand')
+    assert tensor.shape == shape
+
+    tensor = quick_tensor(shape=shape, base='ones')
+    assert tensor.shape == shape
--- hottbox/utils.py
+++ hottbox/utils.py
@@ -3,5 +3,28 @@ This module contains useful functions for the iterative tensor routines
 and data type conversions
 """
 import numpy as np
-from itertools import product
+from functools import reduce
 from .core.structures import Tensor
+
+
+def quick_tensor(shape, base='arange'):
+    """ Create quick tensor
+
+    Parameters
+    ----------
+    shape : tuple
+        Desired shape of the tensor
+    base : str
+        Base function that generate values for the tensor
+    Returns
+    -------
+    tensor : Tensor
+    """
+    base_function = {'arange': np.arange,
+                     'randn': np.random.randn,
+                     'rand': np.random.rand,
+                     'ones': np.ones}.get(base, np.arange)
+    size = reduce(lambda x, y: x * y, shape)
+    array = base_function(size).reshape(shape)
+    tensor = Tensor(array=array)
+    return tensor
